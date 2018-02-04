8825cca4fc8b6ea8e4e777c257a68db99b02070a
Christoph <christoph.fink@gmail.com>
2018-02-04 20:51:38 +0200

    systemd, christophfink.com

--- README.md
+++ README.md
@@ -17,7 +17,7 @@ preparation
 	openssl genrsa 4096 > /var/lib/letsencrypt/account.key
 	openssl genrsa 4096 > /var/lib/letsencrypt/domain.key
 * … and cert signing request (fill in your own domain(s)):
-	openssl req -new -sha256 -key domain.key -subj "/" -reqexts SAN -config <(cat /etc/ssl/openssl.cnf <(printf "[SAN]\nsubjectAltName=DNS:chri.stoph.at,DNS:stoph.at,DNS:austromorph.space,DNS:www.austromorph.space,DNS:peippo.at,DNS:www.peippo.at")) > /var/lib/letsencrypt/domain.csr
+	openssl req -new -sha256 -key domain.key -subj "/" -reqexts SAN -config <(cat /etc/ssl/openssl.cnf <(printf "[SAN]\nsubjectAltName=DNS:chri.stoph.at,DNS:stoph.at,DNS:austromorph.space,DNS:www.austromorph.space,DNS:peippo.at,DNS:www.peippo.at,DNS:christophfink.com,DNS:www.christophfink.com")) > /var/lib/letsencrypt/domain.csr
 * chown all files in /var/lib/letsencrypt/ to letsencrypt:letsencrypt
 * `su - letsencrypt`
 * run `./renew-certificates.sh`
--- renew-certificates.sh
+++ renew-certificates.sh
@@ -3,17 +3,20 @@
 #### prelims ####
 #openssl genrsa 4096 > account.key
 #openssl genrsa 4096 > domain.key
-#openssl req -new -sha256 -key domain.key -subj "/" -reqexts SAN -config <(cat /etc/ssl/openssl.cnf <(printf "[SAN]\nsubjectAltName=DNS:chri.stoph.at,DNS:stoph.at,DNS:austromorph.space,DNS:www.austromorph.space,DNS:peippo.at,DNS:www.peippo.at")) > domain.csr
+#openssl req -new -sha256 -key domain.key -subj "/" -reqexts SAN -config <(cat /etc/ssl/openssl.cnf <(printf "[SAN]\nsubjectAltName=DNS:chri.stoph.at,DNS:stoph.at,DNS:austromorph.space,DNS:www.austromorph.space,DNS:peippo.at,DNS:www.peippo.at,DNS:baikal.peippo.at,DNS:p.peippo.at,DNS:porem.peippo.at,DNS:mansicca.christophfink.com,DNS:musticca.christophfink.com,DNS:christophfink.com,DNS:www.christophfink.com")) > domain.csr
 
 signed="${HOME}/$(date '+signedCertificate_%Y%m%d.crt')"
 chained="${HOME}/$(date '+signedAndChainedCertificate_%Y%m%d.crt')"
 
 acme-tiny --account-key "${HOME}/account.key" --csr "${HOME}/domain.csr" --acme-dir "${HOME}/acme-challenge/" > "${signed}" || exit
-wget -q -O - https://letsencrypt.org/certs/lets-encrypt-x3-cross-signed.pem > "${HOME}/intermediate.pem"
+curl -s https://letsencrypt.org/certs/lets-encrypt-x3-cross-signed.pem -o "${HOME}/intermediate.pem"
 cat "${signed}" "${HOME}/intermediate.pem" > "${chained}"
 ln -sf "${signed}" "${HOME}/signedCertificate.crt"
 ln -sf "${chained}" "${HOME}/signedAndChainedCertificate.crt"
 
-sudo service apache2 restart
-sudo service postfix restart
-sudo service dovecot restart
+chmod 0640 "${signed}" "${chained}"
+
+sudo systemctl restart httpd
+sudo systemctl restart postfix
+sudo systemctl restart dovecot
+sudo systemctl restart postgresql
