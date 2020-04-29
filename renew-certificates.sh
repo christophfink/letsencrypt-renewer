bc9236bda1a644e258d55e914be6c394aacd5ef4
Christoph Fink <root@christophfink.com>
2020-04-29 13:04:51 +0300

    2020-04-29

--- renew-certificates.sh
+++ renew-certificates.sh
@@ -1,22 +1,78 @@
 #!/bin/bash
 
+# “strict mode”
+set -euo pipefail
+IFS=$'\n\t '
+
 #### prelims ####
 #openssl genrsa 4096 > account.key
 #openssl genrsa 4096 > domain.key
-#openssl req -new -sha256 -key domain.key -subj "/" -reqexts SAN -config <(cat /etc/ssl/openssl.cnf <(printf "[SAN]\nsubjectAltName=DNS:chri.stoph.at,DNS:stoph.at,DNS:austromorph.space,DNS:www.austromorph.space,DNS:peippo.at,DNS:www.peippo.at,DNS:baikal.peippo.at,DNS:p.peippo.at,DNS:porem.peippo.at,DNS:mansicca.christophfink.com,DNS:musticca.christophfink.com,DNS:christophfink.com,DNS:www.christophfink.com")) > domain.csr
+#openssl req -new -sha256 -key domain.key -subj "/" -reqexts SAN -config <(cat /etc/ssl/openssl.cnf <(printf "[SAN]\nsubjectAltName=DNS:stoph.at,DNS:chri.stoph.at,DNS:www.chri.stoph.at,DNS:austromorph.space,DNS:dev.austromorph.space,DNS:www.austromorph.space,DNS:peippo.at,www.peippo.at,DNS:baikal.peippo.at,DNS:porem.peippo.at,DNS:christophfink.com,DNS:www.christophfink.com,DNS:mansicca.christophfink.com,DNS:conservationgeography.com,DNS:www.conservationgeography.com")) > domain.csr
 
-signed="${HOME}/$(date '+signedCertificate_%Y%m%d.crt')"
-chained="${HOME}/$(date '+signedAndChainedCertificate_%Y%m%d.crt')"
+declare signed="${HOME}/$(date '+signedCertificate_%Y%m%d.crt')"
+declare chained="${HOME}/$(date '+signedAndChainedCertificate_%Y%m%d.crt')"
 
-acme-tiny --account-key "${HOME}/account.key" --csr "${HOME}/domain.csr" --acme-dir "${HOME}/acme-challenge/" > "${signed}" || exit
-curl -s https://letsencrypt.org/certs/lets-encrypt-x3-cross-signed.pem -o "${HOME}/intermediate.pem"
-cat "${signed}" "${HOME}/intermediate.pem" > "${chained}"
+declare -a domains
+
+# get the domain names of all virtual hosts run by httpd
+domains=( $(apachectl -S | grep namevhost | awk '{print $4}' | sort | uniq) )
+
+# prepend the domains with 'DNS:'
+for i in $(seq 0 $(expr "${#domains[*]}" - 1)); do
+    domains[$i]="DNS:${domains[$i]}"
+done
+
+# join domains using a comma
+declare domains_joined
+domains_joined=$(
+    (
+        IFS=","
+        echo "${domains[*]}"
+    )
+)
+
+# create certificate signing request
+openssl \
+    req \
+    -new \
+    -sha256 \
+    -key "${HOME}/domain.key" \
+    -subj "/" \
+    -reqexts SAN \
+    -config <(
+        cat /etc/ssl/openssl.cnf <(
+            printf "[SAN]\nsubjectAltName=${domains_joined}")
+    ) \
+>"${HOME}/domain.csr"
+
+# request signed certificate
+acme-tiny \
+    --account-key "${HOME}/account.key" \
+    --csr "${HOME}/domain.csr" \
+    --acme-dir "${HOME}/acme-challenge/" \
+>"${signed}"
+
+# download LE’s intermediate certificate and chain it with our newly signed certificate
+curl \
+    -s \
+    -o "${HOME}/intermediate.pem" \
+    https://letsencrypt.org/certs/lets-encrypt-x3-cross-signed.pem
+
+cat \
+    "${signed}" \
+    "${HOME}/intermediate.pem" \
+> "${chained}"
+
+# update symlinks to latest certificates
 ln -sf "${signed}" "${HOME}/signedCertificate.crt"
 ln -sf "${chained}" "${HOME}/signedAndChainedCertificate.crt"
 
+# and adjust the new certificates’ permissions
 chmod 0640 "${signed}" "${chained}"
 
-sudo systemctl restart httpd
-sudo systemctl restart postfix
-sudo systemctl restart dovecot
-sudo systemctl restart postgresql
+# restart services using certificates
+for i in \
+    httpd postfix dovecot prosody turnserver
+do
+    sudo systemctl restart "${i}"
+done
