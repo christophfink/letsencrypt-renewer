80eb4148d7eb7defcdd2a8768e8f260fb18609cc
Christoph Fink <root@christophfink.com>
2021-01-05 17:12:07 +0200

    automatic intermediate chaining, new root ca

--- renew-certificates.sh
+++ renew-certificates.sh
@@ -7,10 +7,9 @@ IFS=$'\n\t '
 #### prelims ####
 #openssl genrsa 4096 > account.key
 #openssl genrsa 4096 > domain.key
-#openssl req -new -sha256 -key domain.key -subj "/" -reqexts SAN -config <(cat /etc/ssl/openssl.cnf <(printf "[SAN]\nsubjectAltName=DNS:stoph.at,DNS:chri.stoph.at,DNS:www.chri.stoph.at,DNS:austromorph.space,DNS:dev.austromorph.space,DNS:www.austromorph.space,DNS:peippo.at,www.peippo.at,DNS:baikal.peippo.at,DNS:porem.peippo.at,DNS:christophfink.com,DNS:www.christophfink.com,DNS:mansicca.christophfink.com,DNS:conservationgeography.com,DNS:www.conservationgeography.com")) > domain.csr
+#openssl req -new -sha256 -key domain.key -subj "/" -addext "subjectAltName = DNS:christophfink.com, DNS:www.christophfink.com, DNS:stoph.at, DNS:chri.stoph.at, DNS:www.chri.stoph.at, DNS:austromorph.space, DNS:dev.austromorph.space, DNS:www.austromorph.space, DNS:peippo.at, DNS:baikal.peippo.at, DNS:www.peippo.at" > domain.csr
 
 declare signed="${HOME}/$(date '+signedCertificate_%Y%m%d.crt')"
-declare chained="${HOME}/$(date '+signedAndChainedCertificate_%Y%m%d.crt')"
 
 declare -a domains
 
@@ -22,11 +21,11 @@ for i in $(seq 0 $(expr "${#domains[*]}" - 1)); do
     domains[$i]="DNS:${domains[$i]}"
 done
 
-# join domains using a comma
+# comma-join domains
 declare domains_joined
 domains_joined=$(
     (
-        IFS=","
+        IFS=", "
         echo "${domains[*]}"
     )
 )
@@ -38,11 +37,7 @@ openssl \
     -sha256 \
     -key "${HOME}/domain.key" \
     -subj "/" \
-    -reqexts SAN \
-    -config <(
-        cat /etc/ssl/openssl.cnf <(
-            printf "[SAN]\nsubjectAltName=${domains_joined}")
-    ) \
+    -addext "subjectAltName = ${domains_joined}" \
 >"${HOME}/domain.csr"
 
 # request signed certificate
@@ -52,23 +47,11 @@ acme-tiny \
     --acme-dir "${HOME}/acme-challenge/" \
 >"${signed}"
 
-# download LE’s intermediate certificate and chain it with our newly signed certificate
-curl \
-    -s \
-    -o "${HOME}/intermediate.pem" \
-    https://letsencrypt.org/certs/lets-encrypt-x3-cross-signed.pem
-
-cat \
-    "${signed}" \
-    "${HOME}/intermediate.pem" \
-> "${chained}"
-
-# update symlinks to latest certificates
-ln -sf "${signed}" "${HOME}/signedCertificate.crt"
-ln -sf "${chained}" "${HOME}/signedAndChainedCertificate.crt"
+# update symlink to latest certificates
+#ln -sf "${signed}" "${HOME}/signedCertificate.crt"
 
 # and adjust the new certificates’ permissions
-chmod 0640 "${signed}" "${chained}"
+chmod 0640 "${signed}"
 
 # restart services using certificates
 for i in \
